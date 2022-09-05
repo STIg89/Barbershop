2bf7c8dd8804adcdb1f8dc036b4ad9b4958fc187
STIg89 <andreystig89@gmail.com>
2022-09-05 03:19:07 +0300

    up

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,14 +1,38 @@
 (() => {
     const refs = {
-        openMenuBtn: document.querySelector("[data-menu-open]"),
-        closeMenuBtn: document.querySelector("[data-menu-close]"),
-        modal: document.querySelector("[data-menu]"),
+        openMenuBtn: document.querySelector('[data-menu-open]'),
+        closeMenuBtn: document.querySelector('[data-menu-close]'),
+        menu: document.querySelector('[data-menu]'),
+        body: document.querySelector('body'),
+        menuList: document.querySelector('.mobile__nav-list'),
     };
-    
-    refs.openMenuBtn.addEventListener("click", toggleMenu);
-    refs.closeMenuBtn.addEventListener("click", toggleMenu);
-    
+
+    refs.openMenuBtn.addEventListener('click', toggleMenu);
+    refs.closeMenuBtn.addEventListener('click', toggleMenu);
+    refs.menuList.addEventListener('click', removeMenu);
+
     function toggleMenu() {
-        refs.modal.classList.toggle("header__mobile-menu--is-hidden");
+        refs.menu.classList.toggle('backdrop--is-hidden');
+        refs.body.classList.toggle('no-scroll');
     }
-    })();
+
+    function removeMenu() {
+        refs.menu.classList.add('backdrop--is-hidden');
+        refs.body.classList.remove('no-scroll');
+    }
+})();
+
+// (() => {
+//     const refs = {
+//         openMenuBtn: document.querySelector("[data-menu-open]"),
+//         closeMenuBtn: document.querySelector("[data-menu-close]"),
+//         modal: document.querySelector("[data-menu]"),
+//     };
+//     
+//     refs.openMenuBtn.addEventListener("click", toggleMenu);
+//     refs.closeMenuBtn.addEventListener("click", toggleMenu);
+//     
+//     function toggleMenu() {
+//         refs.modal.classList.toggle("backdrop--is-hidden");
+//     }
+//     })();
